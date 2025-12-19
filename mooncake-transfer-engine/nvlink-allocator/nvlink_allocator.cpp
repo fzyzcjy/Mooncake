32c1eb9b6ef8a43cf4ac832254e23ae5598fc07e
fzyzcjy <ch271828n@outlook.com>
2025-12-19 11:19:04 +0800

    rm fabric

--- mooncake-transfer-engine/nvlink-allocator/nvlink_allocator.cpp
+++ mooncake-transfer-engine/nvlink-allocator/nvlink_allocator.cpp
@@ -20,7 +20,8 @@ void *mc_nvlink_malloc(ssize_t size, int device, cudaStream_t stream) {
     }
     prop.type = CU_MEM_ALLOCATION_TYPE_PINNED;
     prop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
-    prop.requestedHandleTypes = CU_MEM_HANDLE_TYPE_FABRIC;
+    // NOTE MODIFIED
+    // prop.requestedHandleTypes = CU_MEM_HANDLE_TYPE_FABRIC;
     prop.location.id = currentDev;
     result = cuDeviceGetAttribute(
         &flag, CU_DEVICE_ATTRIBUTE_GPU_DIRECT_RDMA_WITH_CUDA_VMM_SUPPORTED,
